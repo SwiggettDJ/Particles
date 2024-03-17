895769940ae40c2913f378835b7b0cce6fa29d36
SwiggettDJ <swiggettdj@gmail.com>
2024-03-16 22:52:21 -0600

    Made Ice VFX and Heal VFX

---
+++ Assets/CameraShake.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private CinemachineImpulseSource impulse;
+
+    [SerializeField] private float preDelay;
+    [SerializeField] private float postDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        impulse = GetComponent<CinemachineImpulseSource>();
+        StartCoroutine(Shake());
+    }
+
+    private IEnumerator Shake()
+    {
+        yield return new WaitForSeconds(preDelay);
+        impulse.GenerateImpulse();
+        yield return new WaitForSeconds(postDelay);
+        StartCoroutine(Shake());
+    }
+}
