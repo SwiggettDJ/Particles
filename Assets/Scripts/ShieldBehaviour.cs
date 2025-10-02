24460615878fce259d96110475cccd7ffcaf48b5
SwiggettDJ <swiggettdj@gmail.com>
2025-10-02 14:12:06 -0600

    Added in sunshield vfx

---
+++ Assets/Scripts/ShieldBehaviour.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class ShieldBehaviour : MonoBehaviour
+{
+    private VisualEffect shieldVFX;
+    private Animator lightAnimator;
+    
+    void Start()
+    {
+        shieldVFX = GetComponent<VisualEffect>();
+        lightAnimator = GetComponentInChildren<Animator>();
+    }
+
+    public void BreakShield()
+    {
+        StartCoroutine(Delay(3.5f));
+    }
+
+    IEnumerator Delay(float timeBeforeReset)
+    {
+        shieldVFX.SendEvent("OnBreakStart");
+        shieldVFX.SetBool("Kill Ring", true);
+        
+        float breakDuration = shieldVFX.GetFloat("Break Duration");
+        lightAnimator.SetFloat("ExplosionMultiplier", 1/breakDuration);
+        lightAnimator.SetTrigger("Explode");
+        
+        yield return new WaitForSeconds(breakDuration);
+        
+        shieldVFX.SendEvent("OnBreakEnd");
+        shieldVFX.SetBool("Kill Particles", true);
+        
+        yield return new WaitForSeconds(timeBeforeReset);
+        
+        Reset();
+    }
+
+    private void Reset()
+    {
+        shieldVFX.SetBool("Kill Particles", false);
+        shieldVFX.SetBool("Kill Ring", false);
+        shieldVFX.Reinit();
+        lightAnimator.Rebind();
+    }
+}
